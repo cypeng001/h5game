2148c33eb681c05e772c803cf0fe36a9b1d84cb0
cypeng <286816680@qq.com>
2018-02-01 11:48:32 +0800

    improve ConfigMgr.ts & configzip.py, support data/manifest.json

--- client/project/minitools/configzip.py
+++ client/project/minitools/configzip.py
@@ -1,16 +1,44 @@
 import os
 import zipfile
 
-rootdir = "../resource/config"
-zippath = "../resource/config.zip"
+ROOT_DIR = '../resource/config'
+ZIP_PATH = '../resource/config.zip'
+BASE_DATA_DIR = '../resource/config/data'
 
-f = zipfile.ZipFile(zippath, 'w', zipfile.ZIP_DEFLATED)
+TAB_STR = '    '
+CR_STR = '\n'
 
-for dirpath, dirnames, filenames in os.walk(rootdir):  
-    for filename in filenames: 
-        filepath = dirpath + "/" + filename
-        filepath = filepath.replace("\\", "/")
-        arcname = filepath[len(rootdir) + 1: len(filepath)]
-        f.write(filepath, arcname)  
+def zipConfig():
+    f = zipfile.ZipFile(ZIP_PATH, 'w', zipfile.ZIP_DEFLATED)
 
-f.close()
+    for dirpath, dirnames, filenames in os.walk(ROOT_DIR):  
+        for filename in filenames: 
+            filepath = dirpath + '/' + filename
+            filepath = filepath.replace('\\', '/')
+            arcname = filepath[len(ROOT_DIR) + 1: len(filepath)]
+            f.write(filepath, arcname)
+
+    f.close()
+
+def exportManifest(dir):
+    fo = open(dir + '/manifest.json', 'w')
+
+    manifest = []
+    for dirpath, dirnames, filenames in os.walk(dir):  
+        for filename in filenames: 
+            if filename != 'manifest.json':
+                manifest.append(filename[0:len(filename)-5])
+
+    fo.write('[' + CR_STR)
+    for k in range(len(manifest)):
+        fo.write('"' + manifest[k] + '"')
+        if k < len(manifest) - 1:
+            fo.write(',')
+        fo.write(CR_STR)
+    fo.write(']')
+
+    fo.close()
+
+if __name__ == '__main__':
+    exportManifest(BASE_DATA_DIR)
+    zipConfig()
--- client/project/src/utils/ConfigMgr.ts
+++ client/project/src/utils/ConfigMgr.ts
@@ -1,8 +1,7 @@
 class ConfigMgr {
     private static _instance: ConfigMgr = null;
     
-    public static getInstance(): ConfigMgr
-    {
+    public static getInstance(): ConfigMgr {
         if(!this._instance)
         {
             this._instance = new ConfigMgr();
@@ -12,46 +11,43 @@ class ConfigMgr {
 
     private _configMap: Object = {};
 
-    private _preloadConfigFiles = [
-        "city"
-        ];
+    private _baseConfigFiles: Array<string> = null;
 
-    public init(): void
-    {
+    public init(): void {
         var zip = new JSZip(RES.getRes("config_zip"));
-
-        for(var i in this._preloadConfigFiles)
-        {
-            var configName = this._preloadConfigFiles[i];
-            var config = JSON.parse(zip.file("data/" + configName + ".json").asText());
-            this.registerConfig(configName, config);
-        }
-        
+        this.initBaseManifest(zip);
+        this.initBaseCnf(zip);
         this.initMapCnf(zip);
-
         RES.destroyRes("config_zip");
     }
 
-    private initMapCnf(zip: JSZip): void
-    {
+    private initBaseManifest(zip: JSZip): void {
+        this._baseConfigFiles = JSON.parse(zip.file("data/manifest.json").asText());
+    }
+
+    private initBaseCnf(zip: JSZip): void {
+        for(var i in this._baseConfigFiles) {
+            var configName = this._baseConfigFiles[i];
+            var config = JSON.parse(zip.file("data/" + configName + ".json").asText());
+            this.registerConfig(configName, config);
+        }
+    }
+
+    private initMapCnf(zip: JSZip): void {
         var city_cnfs = ConfigMgr.getInstance().getConfig("city");
-        for (var map_id in city_cnfs)
-        {
+        for (var map_id in city_cnfs) {
             var configName = "map_" + map_id;
             var config = JSON.parse(zip.file("map/" + map_id + ".json").asText());
             this.registerConfig(configName, config);
         }
     }
 
-    public registerConfig(configName: string, config: any): Boolean
-    {
-        if(!config)
-        {
+    public registerConfig(configName: string, config: any): Boolean {
+        if(!config) {
             return false;
         }
 
-        if(this._configMap[configName])
-        {
+        if(this._configMap[configName]) {
             console.log("ConfigMgr_registerConfig config already exist", configName);
             return false;
         }
@@ -61,18 +57,15 @@ class ConfigMgr {
         return true;
     }
 
-    public getConfig(configName: string): any
-    {
+    public getConfig(configName: string): any {
         var config = this._configMap[configName];
-        if(!config)
-        {
+        if(!config) {
             console.log("ConfigMgr_getConfig invalid configName", configName);
         }
         return config;
     }
 
-    public getMapConfig(mapID: number): any
-    {
+    public getMapConfig(mapID: number): any {
         var configName = "map_" + mapID;
         return this.getConfig(configName);
     }
