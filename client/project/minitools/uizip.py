280c2d06436b7186ee5c7a7d10b2e9a29cde694a
cypeng <286816680@qq.com>
2018-02-01 12:02:33 +0800

    uizip.py init commit, support zip ui json

---
+++ client/project/minitools/uizip.py
@@ -0,0 +1,45 @@
+import os
+import zipfile
+
+ROOT_DIR = '../resource/ui'
+ZIP_PATH = '../resource/ui.zip'
+BASE_DATA_DIR = '../resource/ui'
+
+TAB_STR = '    '
+CR_STR = '\n'
+
+def zipConfig():
+    f = zipfile.ZipFile(ZIP_PATH, 'w', zipfile.ZIP_DEFLATED)
+
+    for dirpath, dirnames, filenames in os.walk(ROOT_DIR):  
+        for filename in filenames: 
+            if filename[-5:len(filename)] == '.json':
+                filepath = dirpath + '/' + filename
+                filepath = filepath.replace('\\', '/')
+                arcname = filepath[len(ROOT_DIR) + 1: len(filepath)]
+                f.write(filepath, arcname)
+
+    f.close()
+
+def exportManifest(dir):
+    fo = open(dir + '/manifest.json', 'w')
+
+    manifest = []
+    for dirpath, dirnames, filenames in os.walk(dir):  
+        for filename in filenames: 
+            if filename[-5:len(filename)] == '.json' and filename != 'manifest.json':
+                manifest.append(filename[0:len(filename)-5])
+
+    fo.write('[' + CR_STR)
+    for k in range(len(manifest)):
+        fo.write('"' + manifest[k] + '"')
+        if k < len(manifest) - 1:
+            fo.write(',')
+        fo.write(CR_STR)
+    fo.write(']')
+
+    fo.close()
+
+if __name__ == '__main__':
+    exportManifest(BASE_DATA_DIR)
+    zipConfig()
