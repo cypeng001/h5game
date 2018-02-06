68663d8c6c1a319bdd33d500447689c79bdf52d9
cypeng <286816680@qq.com>
2018-02-06 14:20:11 +0800

    separate ConfigMgr to cnf libs

---
+++ client/gamelibs/bin/cnf/cnf.d.ts
@@ -0,0 +1,13 @@
+declare class ConfigMgr {
+    private static _instance;
+    static getInstance(): ConfigMgr;
+    private _configMap;
+    private _baseConfigFiles;
+    init(): void;
+    private initBaseManifest(zip);
+    private initBaseCnf(zip);
+    private initMapCnf(zip);
+    registerConfig(configName: string, config: any): Boolean;
+    getConfig(configName: string): any;
+    getMapConfig(mapID: number): any;
+}
